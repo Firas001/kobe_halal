0b2d92e5bc419af2871db90ea12136208a924bfa
Firas001 <al.dweni001@gmail.com>
2026-02-01 01:22:57 +0900

    fix: add pagination

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Place, Category 
 from django.contrib.auth import logout
+from django.core.paginator import Paginator 
 
 def restaurant_list(request):
-    places = Place.objects.all()
+    places_list = Place.objects.all().order_by('-created_at') 
     categories = Category.objects.all()
 
     category_id = request.GET.get('category')
     if category_id:
-        places = places.filter(category_id=category_id)
+        places_list = places_list.filter(category_id=category_id)
+
+    paginator = Paginator(places_list, 10) 
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request, 'restaurants/list.html', {
-        'restaurants': places,
+        'restaurants': page_obj, 
         'categories': categories,
         'selected_category': int(category_id) if category_id else None
     })
