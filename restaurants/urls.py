98cbd3e3672bf5cfd05f06716dca5332f118d0a6
Firas001 <al.dweni001@gmail.com>
2026-01-31 18:38:42 +0900

    fix: add views

---
+++ restaurants/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.restaurant_list, name='list'),
+    path('restaurant/<int:pk>/', views.restaurant_detail, name='detail'),
+]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Restaurant, Category
 
-# Create your views here.
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+    categories = Category.objects.all()
+
+    category_id = request.GET.get('category')
+    if category_id:
+        restaurants = restaurants.filter(category_id=category_id)
+
+    return render(request, 'restaurants/list.html', {
+        'restaurants': restaurants,
+        'categories': categories,
+        'selected_category': int(category_id) if category_id else None
+    })
+
+def restaurant_detail(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    return render(request, 'restaurants/detail.html', {'restaurant': restaurant})
